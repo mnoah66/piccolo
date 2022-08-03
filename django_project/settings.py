311a91b801b1ba078b3829c57aa39b572da4ec3f
Martin Noah <mnoah66@gmail.com>
2022-08-03 10:25:28 -0400

    typo in allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = env.bool("DEBUG", default=False)
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "*.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", ".herokuapp.com"]
 
 # APPS
 # ------------------------------------------------------------------------------
